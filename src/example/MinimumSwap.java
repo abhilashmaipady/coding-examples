3aa53262fcb2a92f08f64248901c0385a04ba44e
abhilashmaipady <abhilashmaipady@gmail.com>
2019-11-06 22:11:30 +0530

    Minimum swap efficient code

--- src/example/MinimumSwap.java
+++ src/example/MinimumSwap.java
@@ -1,97 +1,72 @@
 package example;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Scanner;
-import java.util.Stack;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+class Element {
+       int value;
+       int oldPosition;
+       int actualPosition;
+       boolean visited;
+}
+
+class SortElement implements Comparator<Element> {
+
+	@Override
+	public int compare(Element e1, Element e2) {
+		// TODO Auto-generated method stub
+		return e1.value - e2.value;
+	}
+	
+}
 
 public class MinimumSwap {
 
-	private static Stack<Integer> stack;
 	static int minimumSwaps(int[] arr) {
 		int noOfSwap = 0;
-		int index = 0;
-		int swapIndex;
-		while(true) {
-			stack = new Stack<Integer>();
-			index = findOdd(arr);
-			if(index == arr.length-1) break;
-			swapIndex = findSwapItem(arr, arr[index], index+1);
-//			if(swapIndex == arr.length || index == swapIndex) break;
-			swap(arr, index, swapIndex);
-			noOfSwap++;
+		List<Element> elementList = constructArray(arr);
+		for(Element e: elementList) {
+			if(e.visited) continue;
+			noOfSwap += findCycle(elementList, e);
 		}
-
-		
 		return noOfSwap;
     }
 	
-	private static int findOdd(int[] arr) {
-		int index = 0;
-		for(int i=index+1;i<arr.length;i++) {
-			if(arr[index] < arr[i]) index = i;
-			else break;
-		}
-		return index;
-	}
-	
-	private static void swap(int[] arr, int first, int second) {
-		int temp = arr[first];
-		arr[first] = arr[second];
-		arr[second] = temp;
-	}
-	
-	private static int findSwapItem(int[] arr, int item, int index) {
-		int top = index;
-		for(int i=index;i<arr.length;i++) {
-			if(item > arr[i]) {
-				top = i;
-				if(!insertStack(arr[i])) {
-					return i;
-				}
-			} else {
-				return top;
+	private static int findCycle(List<Element> arr, Element e) {
+		Element item = e;
+		List<Element> cycle = new ArrayList<Element>();
+		while(true) {
+			item = arr.get(item.oldPosition);
+			item.visited = true;
+			cycle.add(item);
+			if(item.value == e.value) {
+				return cycle.size() - 1;
 			}
 		}
-		return top;
 	}
 	
-	private static boolean insertStack(int item) {
-		if(!stack.empty()) {
-			int top = stack.peek();
-			if(item < top) return false;
-		}
-		stack.push(item);
-		return true;
+	private static List<Element> constructArray(int[] arr) {
+		List<Element> eleList = new ArrayList<Element>();
+			for(int i=0;i < arr.length; i++) {
+		    	Element ele = new Element();
+		        ele.value = arr[i];
+		        ele.oldPosition = i;
+		        ele.visited = false;
+		        eleList.add(ele);
+			}
+		    Collections.sort(eleList, new SortElement());
+		    int index = 0;
+		    for(Element e:eleList) {
+		    	e.actualPosition = index++;
+		    }
+		    return eleList;
 	}
 	
-	private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
-
-        int n = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int[] arr = new int[n];
-
-        String[] arrItems = scanner.nextLine().split(" ");
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        for (int i = 0; i < n; i++) {
-            int arrItem = Integer.parseInt(arrItems[i]);
-            arr[i] = arrItem;
-        }
-
-        int res = minimumSwaps(arr);
-
-        bufferedWriter.write(String.valueOf(res));
-        bufferedWriter.newLine();
-
-        bufferedWriter.close();
-
-        scanner.close();
+        int[] arr = new int[] {3, 7, 6, 9, 1, 8, 10, 4, 2, 5};
+        System.out.println(minimumSwaps(arr));
     }
 
 }
